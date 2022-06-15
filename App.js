bab7883b7658c6791a66fda7707c5e93703fad47
Molikkidd <mzkidd5@gmail.com>
2022-06-14 17:53:22 -0700

    add redux store

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from "react-redux";
+import { store } from './store';
 
+// setup redux 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Lets Build Uber!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // add provider wrapper around App view
+    <Provider store={store}>
+      <View style={styles.container}>
+        <Text>Lets Build Uber!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </Provider>
   );
 }
 
---
+++ slices/navSlice.js
@@ -0,0 +1,41 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+// setup initial destination values
+
+const initialState = {
+    origin: null,
+    destination: null,
+    travelTimeInformation: null
+}
+
+// create slice
+
+const navSlice = createSlice({
+    name: 'nav',
+    initialState,
+    reducer: {
+        // where it came from
+        setOrigin: (state, action) => {
+            state.origin = action.payload;
+        },
+        // where its going
+        setDestination: (state, action) => {
+            state.origin = action.payload
+        },
+        // how long it takes to get there
+        setTravelTimeInformation: (state, action) => {
+            state.travelTimeInformation = action.payload
+        },
+    },
+});
+
+// exporting each action so the rest of the app can access these methods
+export const { setOrigin, setDestination, setTravelTimeInformation} = navSlice.actions;
+
+// import data from the data layer
+// Selectors
+export const selectOrigin = (state) => state.nav.origin;
+export const selectDestination = (state) => state.nav.destination;
+export const selectTravelTimeInformation = (state) => state.nav.travelTimeInformation;
+// connect to 
+export default navSlice.reducer;
