fa1e0b05365c7c6e96c44754e0733314d7f2bf5a
Molikkidd <mzkidd5@gmail.com>
2022-06-16 16:29:52 -0700

    add google maps auto complete

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,15 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    // add dotenv plugin to app
+    plugins: [
+      [
+        "module:react-native-dotenv",
+        {
+          moduleName: "@env",
+          path: ".env",
+        },
+      ],
+    ],
   };
 };
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,6 +2,9 @@ import { StyleSheet, Text, View , SafeAreaView, Image} from 'react-native'
 import React from 'react'
 import tw from 'tailwind-react-native-classnames'
 import NavOptions from '../components/NavOptions'
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import {GOOGLE_MAPS_APIKEY} from "@env"
+
 // add tailwind
 
 const HomeScreen = () => {
@@ -17,6 +20,19 @@ const HomeScreen = () => {
           source={{
               uri: "https://links.papareact.com/gzs"
           }}/>
+          <GooglePlacesAutocomplete
+          placeholder='Where From?'
+          nearbyPlacesAPI='GooglePlacesSearch'
+          debounce={400}
+          styles={{
+            container: {flex: 0,},
+            textInput: {fontSize: 18,}
+          }}
+          query={{
+            key: GOOGLE_MAPS_APIKEY,
+            language: "en", 
+          }}
+          />
           <NavOptions/>
       </View>
     </SafeAreaView>
