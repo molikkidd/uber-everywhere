3db8fcf85babcbdf30b9f1e1b5f4bccb4fc4d190
Molikkidd <mzkidd5@gmail.com>
2022-06-17 14:05:16 -0700

    add car icons and travel time

--- components/RideOptionsCard.js
+++ components/RideOptionsCard.js
@@ -1,11 +1,69 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import tw from 'tailwind-react-native-classnames'
+import { FlatList, TouchableOpacity } from 'react-native-gesture-handler'
+import { Icon, Image } from 'react-native-elements'
+import { useNavigation } from '@react-navigation/native'
+
+
+const data = [
+  {
+    id: "Uber-X-123",
+    title: "UberX",
+    multiplier: 1,
+    image: "https://links.papareact.com/3pn",
+  },
+  {
+    id: "Uber-X-456",
+    title: "Uber XL",
+    multiplier: 1.2,
+    image: "https://links.papareact.com/5w8",
+  },
+  {
+    id: "Uber-LUX-789",
+    title: "Uber LUX",
+    multiplier: 1.75,
+    image: "https://links.papareact.com/7pf",
+  },
+];
 
 const RideOptionsCard = () => {
+  const navigation = useNavigation();
+
   return (
-    <View>
-      <Text>RideOptionsCard</Text>
-    </View>
+    <SafeAreaView style={tw`bg-white flex-grow`}>
+      <View>
+        <TouchableOpacity 
+        onPress={() => navigation.navigate("NavigateCard")}
+        style={tw`absolute top-3 left-5 z-50 p-3 rounded-full`}>
+          <Icon name="chevron-left" type="fontawesome"/>
+        </TouchableOpacity>
+      <Text style={tw`text-center py-2 text-xl`}>Select a Ride</Text>
+      </View>
+      <FlatList
+      data={data}
+      keyExtractor={(item) => item.id}
+      renderItem={({item : { id, title, multiplier, image}, item}) => (
+        <TouchableOpacity
+        style={tw`flex-row items-center justify-between px-10`}
+        >
+            <Image
+            style={{
+              width: 100,
+              height: 100,
+              resizeMode: 'contain',
+            }}
+            source={{uri: image}}
+            />
+            <View>
+                <Text style={tw`font-semibold text-lg`}>{title}</Text>
+                <Text style={tw`text-gray-500`}>Travel time...</Text>
+            </View>
+        </TouchableOpacity>
+      )}
+      />
+    </SafeAreaView>
   )
 }
 
