81f963ccf48306892584a2b874d7b932adb4f76f
Molikkidd <mzkidd5@gmail.com>
2022-06-16 20:53:54 -0700

    update navigate card options

--- components/NavigateCard.js
+++ components/NavigateCard.js
@@ -3,18 +3,41 @@ import { StyleSheet, Text, View ,SafeAreaView} from 'react-native'
 import tw from 'tailwind-react-native-classnames'
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 import {GOOGLE_MAPS_APIKEY} from "@env"
+import { useDispatch } from 'react-redux';
+import { useNavigation } from '@react-navigation/native';
+import { setDestination, setOrigin } from '../slices/navSlice';
 
 const NavigateCard = () => {
+    const dispatch = useDispatch();
+    const navigation = useNavigation();
+
   return (
-    <SafeAreaView styles={tw`bg-white flex-1`}>
+    <SafeAreaView style={tw`bg-white flex-1`}>
       <Text style={tw`text-center py-5 text-xl`}>Zao Shang Hao</Text>
-        <View styles={tw`border-t border-gray-200 flex-shrink`}>
+        <View style={tw`border-t border-gray-200 flex-shrink`}>
             <View>
                 <GooglePlacesAutocomplete
                 placeholder='Where to?'
                 nearbyPlacesAPI='GooglePlacesSearch'
                 debounce={400}
                 styles={toInputBoxStyles}
+                fetchDetails={true}
+                enablePoweredByContainer={false}
+                query={{
+                    key: GOOGLE_MAPS_APIKEY,
+                    language: "en", 
+                  }}
+                  returnKeyType={"search"}
+                  onPress={(data, details = null) => {
+                    // add location coordinates to redux store        
+                      dispatch(
+                        setDestination({
+                        location: details.geometry.location,
+                        description: data.description,
+                         })
+                      );
+                      navigation.navigate("RideOptionsCard");
+                  }}
                 />
             </View>
         </View>
