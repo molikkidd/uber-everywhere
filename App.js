6b309fb219e922cf923410bdce4776e5e67a3c98
Molikkidd <mzkidd5@gmail.com>
2022-06-17 13:15:38 -0700

    go to rides page logic

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { KeyboardAvoidingView, Platform } from 'react-native';
 import { Provider } from "react-redux";
 import HomeScreen from './screens/HomeScreen';
 import { store } from './store';
@@ -16,35 +16,34 @@ export default function App() {
   return (
     // add provider wrapper around App view
     <Provider store={store}>
-      <NavigationContainer>    
+      <NavigationContainer>  
         <SafeAreaProvider >
-          <Stack.Navigator>
-            <Stack.Screen 
-            name="HomeScreen"
-            component={HomeScreen}
-            options={{
-              headerShown: false,
-            }}
-            />
-            <Stack.Screen 
-            name="MapScreen"
-            component={MapScreen}
-            options={{
-              headerShown: false,
-            }}
-            />
-          </Stack.Navigator>
+          <KeyboardAvoidingView 
+          behavior={Platform.OS === "ios" ?  "padding " : "height"}
+          style={{flex: 1}}
+          keyboardVerticalOffset={Platform.OS === "ios" ? -64 : 0}
+          >
+            <Stack.Navigator>
+              <Stack.Screen 
+              name="HomeScreen"
+              component={HomeScreen}
+              options={{
+                headerShown: false,
+              }}
+              />
+              <Stack.Screen 
+              name="MapScreen"
+              component={MapScreen}
+              options={{
+                headerShown: false,
+              }}
+              />
+            </Stack.Navigator>
+          </KeyboardAvoidingView>  
         </SafeAreaProvider>
       </NavigationContainer>
     </Provider>
     );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
--- components/NavigateCard.js
+++ components/NavigateCard.js
@@ -6,6 +6,9 @@ import {GOOGLE_MAPS_APIKEY} from "@env"
 import { useDispatch } from 'react-redux';
 import { useNavigation } from '@react-navigation/native';
 import { setDestination, setOrigin } from '../slices/navSlice';
+import NavFavourites from './NavFavourites';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { Icon } from 'react-native-elements';
 
 const NavigateCard = () => {
     const dispatch = useDispatch();
@@ -39,6 +42,24 @@ const NavigateCard = () => {
                       navigation.navigate("RideOptionsCard");
                   }}
                 />
+            </View>
+            <NavFavourites/>
+
+            <View style={tw`flex-row bg-white justify-evenly py-2 mt-auto border-t border-gray-100`}>
+              <TouchableOpacity 
+              style={tw`flex flex-row justify-between bg-black w-24 px-4 py-3 rounded-full`}
+              onPress={() => navigation.navigate("RideOptionsCard")}
+              >
+                <Icon name='car' type='font-awesome' color="white" size={16}/>
+                <Text style={tw`text-white text-center`}>Rides</Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity 
+              style={tw`flex flex-row w-24 px-4 justify-between py-3 rounded-full`}
+              >
+                <Icon name='fast-food-outline' type='ionicon' color="black" size={16}/>
+                <Text style={tw`text-center`}>Eats</Text>
+              </TouchableOpacity>
             </View>
         </View>
     </SafeAreaView>
