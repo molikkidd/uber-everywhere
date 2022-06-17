03fe482b0cbe64ac6784b45bf5b46943bebed3bf
Molikkidd <mzkidd5@gmail.com>
2022-06-17 11:52:22 -0700

    add favourite icons to home screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -6,6 +6,7 @@ import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplet
 import {GOOGLE_MAPS_APIKEY} from "@env"
 import { useDispatch } from 'react-redux';
 import { setDestination, setOrigin } from '../slices/navSlice';
+import NavFavourites from '../components/NavFavourites';
 // add tailwind
 
 const HomeScreen = () => {
@@ -53,6 +54,7 @@ const HomeScreen = () => {
           }}
           />
           <NavOptions/>
+          <NavFavourites/>
       </View>
     </SafeAreaView>
   )
