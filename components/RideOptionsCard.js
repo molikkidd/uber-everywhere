4889105546ffa38019df1419efbff77f8a4edfc4
Molikkidd <mzkidd5@gmail.com>
2022-06-18 09:35:33 -0700

    add travel time information to select a ride

--- components/RideOptionsCard.js
+++ components/RideOptionsCard.js
@@ -1,10 +1,12 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, {useState} from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import tw from 'tailwind-react-native-classnames'
 import { FlatList, TouchableOpacity } from 'react-native-gesture-handler'
 import { Icon, Image } from 'react-native-elements'
 import { useNavigation } from '@react-navigation/native'
+import { useSelector } from 'react-redux'
+import { selectTravelTimeInformation } from '../slices/navSlice'
 
 
 const data = [
@@ -30,23 +32,26 @@ const data = [
 
 const RideOptionsCard = () => {
   const navigation = useNavigation();
-
+  const [selected, setSelected] = useState(null);
+  const travelTimeInformation = useSelector(selectTravelTimeInformation)
   return (
     <SafeAreaView style={tw`bg-white flex-grow`}>
       <View>
         <TouchableOpacity 
         onPress={() => navigation.navigate("NavigateCard")}
-        style={tw`absolute top-3 left-5 z-50 p-3 rounded-full`}>
+        style={tw`absolute left-5 z-50 p-3 rounded-full`}>
           <Icon name="chevron-left" type="fontawesome"/>
         </TouchableOpacity>
-      <Text style={tw`text-center py-2 text-xl`}>Select a Ride</Text>
+      <Text style={tw`text-center py-2 text-xl`}>Select a Ride - {travelTimeInformation?.distance.text}</Text>
       </View>
       <FlatList
       data={data}
       keyExtractor={(item) => item.id}
       renderItem={({item : { id, title, multiplier, image}, item}) => (
         <TouchableOpacity
-        style={tw`flex-row items-center justify-between px-10`}
+        // set selected item to use state and add gray bg to selected
+        onPress={() => setSelected(item)}
+        style={tw`flex-row items-center justify-between px-10 ${id === selected?.id && "bg-gray-200"}`}
         >
             <Image
             style={{
@@ -56,13 +61,23 @@ const RideOptionsCard = () => {
             }}
             source={{uri: image}}
             />
-            <View>
-                <Text style={tw`font-semibold text-lg`}>{title}</Text>
+            <View style={tw`-ml-6`}>
+                <Text style={tw`font-semibold text-xl`}>{title}</Text>
                 <Text style={tw`text-gray-500`}>Travel time...</Text>
             </View>
+            <Text style={tw`text-xl`}> $99</Text>
         </TouchableOpacity>
       )}
       />
+      <View>
+        <TouchableOpacity 
+        disabled={!selected}
+        style={tw`bg-black py-3 m-3 ${!selected && "bg-gray-300"}`}>
+          <Text style={tw`text-center text-white text-xl`}>
+            Choose {selected?.title}
+          </Text>
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   )
 }
